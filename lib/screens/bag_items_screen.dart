992b87e1ccd13e48cb74604461846c7e609eed5c
Dhruvi <dhruvi244210@gmail.com>
2023-03-09 19:44:30 +0530

    cart page completed

--- lib/screens/bag_items_screen.dart
+++ lib/screens/bag_items_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:velocity_x/velocity_x.dart';
+
+import '../utils/routes.dart';
+import '../widgets/colors.dart';
 
 class BagItemsScreen extends StatelessWidget {
   const BagItemsScreen({super.key});
@@ -6,8 +10,70 @@ class BagItemsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("bag items"),
+      backgroundColor: MyColors.myBackgroundColor,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(28.0, 50.0, 28, 0),
+          child: Container(
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    Text(
+                      "Bag Items",
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                          fontFamily: "Inter"),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 85,
+                ),
+                Center(
+                  child: Image.asset(
+                    "assets/images/emptybag.png",
+                    height: 170,
+                    width: 170,
+                  ),
+                ),
+                SizedBox(
+                  height: 36,
+                ),
+                Center(
+                  child: Text(
+                    "Your bag is lonely",
+                    style: TextStyle(fontWeight: FontWeight.w700, fontSize: 21),
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Center(
+                  child: Text("Add some items to cheer it up",
+                      style: TextStyle(fontSize: 15)),
+                ),
+                SizedBox(
+                  height: 32,
+                ),
+                InkWell(
+                  onTap: () => Navigator.of(context)
+                      .pushNamed(MyRoute.bottomNavbarRoute),
+                  child: Container(
+                    height: 45,
+                    width: context.screenWidth,
+                    decoration: BoxDecoration(
+                        color: MyColors.myPrimaryColor,
+                        borderRadius: BorderRadius.circular(38)),
+                    child: "Go To Home".text.white.make().centered(),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
